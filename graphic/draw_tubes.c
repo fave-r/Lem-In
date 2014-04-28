9c6a3f672e694eb90a88c9f9ad6056f5758712fe
lopez_t <lopez_t@epitech.eu>
2014-04-28 14:20:36 +0200

    Graphique

--- graphic/draw_tubes.c
+++ graphic/draw_tubes.c
@@ -5,7 +5,7 @@
 ** Login   <thibaut.lopez@epitech.net>
 ** 
 ** Started on  Fri Apr 25 11:33:21 2014 Thibaut Lopez
-** Last update Sat Apr 26 04:29:00 2014 Thibaut Lopez
+** Last update Sun Apr 27 21:38:45 2014 Thibaut Lopez
 */
 
 #include "graphic.h"
@@ -13,17 +13,27 @@
 void	draw_tubes(SDL_Rect *f_box, SDL_Rect *s_box, SDL_Surface *arena)
 {
   int	i;
+  int	iterat;
 
   i = 0;
-  f_box->x += 15;
-  f_box->y += 20;
-  s_box->x += 15;
-  s_box->y += 20;
-  while (i < 12)
+  iterat = ((f_box->x > s_box->x && f_box->y > s_box->y) ||
+	    (f_box->x < s_box->x && f_box->y < s_box->y)) ? 1 : -1;
+  f_box->x += 25 + iterat * 4;
+  f_box->y += 21;
+  s_box->x += 25 + iterat * 4;
+  s_box->y += 21;
+  while (i < 8)
     {
       draw_line(f_box, s_box, arena);
-      f_box->x++;
-      s_box->x++;
+      f_box->x -= iterat;
+      s_box->x -= iterat;
+      draw_line(f_box, s_box, arena);
+      f_box->y++;
+      s_box->y++;
       i++;
     }
+  f_box->x -= 25 + iterat * 4;
+  f_box->y -= 29;
+  s_box->x -= 25 + iterat * 4;
+  s_box->y -= 29;
 }
--- graphic/init_graphic.c
+++ graphic/init_graphic.c
@@ -5,12 +5,12 @@
 ** Login   <thibaut.lopez@epitech.net>
 ** 
 ** Started on  Wed Mar 19 09:16:50 2014 Thibaut Lopez
-** Last update Sat Apr 26 03:08:54 2014 Thibaut Lopez
+** Last update Mon Apr 28 14:19:18 2014 Thibaut Lopez
 */
 
 #include "graphic.h"
 
-int	my_pause(SDL_Surface *screen)
+int	my_pause(SDL_Surface *screen, SDL_Surface *arena, int x, int y, int posx, int posy)
 {
   int		wait;
   SDL_Event	event;
@@ -27,40 +27,151 @@ int	my_pause(SDL_Surface *screen)
       else if (event.key.keysym.sym == SDLK_SPACE &&
 	       event.type == SDL_KEYDOWN)
 	wait = 1;
+      else if (event.key.keysym.sym == SDLK_DOWN && posy + y - 10 >= 1000)
+	{
+	  y -= 10;
+	  position.x = x;
+	  position.y = y;
+	  SDL_BlitSurface(arena, NULL, screen, &position);
+	  SDL_Flip(screen);
+	}
+      else if (event.key.keysym.sym == SDLK_UP && y + 10 <= 0)
+	{
+	  y += 10;
+	  position.x = x;
+	  position.y = y;
+	  SDL_BlitSurface(arena, NULL, screen, &position);
+	  SDL_Flip(screen);
+	}
+      else if (event.key.keysym.sym == SDLK_RIGHT && posx + x - 10 >= 1000)
+	{
+	  x -= 10;
+	  position.x = x;
+	  position.y = y;
+	  SDL_BlitSurface(arena, NULL, screen, &position);
+	  SDL_Flip(screen);
+	}
+      else if (event.key.keysym.sym == SDLK_LEFT && x + 10 <= 0)
+	{
+	  x += 10;
+	  position.x = x;
+	  position.y = y;
+	  SDL_BlitSurface(arena, NULL, screen, &position);
+	  SDL_Flip(screen);
+	}
     }
   return (0);
+}
+
+void	fill_arena(SDL_Surface *arena, int x, int y)
+{
+  SDL_Rect	position;
+  SDL_Surface	*image;
+
+  image = SDL_LoadBMP("dirt_texture.bmp"); 
+  if (image == NULL)
+    {
+      SDL_FillRect(arena, NULL, SDL_MapRGB(arena->format, 150, 150, 150));
+      return;
+    }
+  position.x = 0;
+  position.y = 0;
+  SDL_BlitSurface(image, NULL, arena, &position);
+  if (position.h != 50 || position.w != 50)
+    SDL_FillRect(arena, NULL, SDL_MapRGB(arena->format, 150, 150, 150));
+  while (position.h == 50 && position.w == 50 && position.y < y)
+    {
+      SDL_BlitSurface(image, NULL, arena, &position);
+      position.x += 50;
+      if (position.x == x)
+	{
+	  position.x = 0;
+	  position.y += 50;
+	}
+    }
+  SDL_FreeSurface(image);
 }
 
 int	init_graphic()
 {
   SDL_Surface	*screen;
+  SDL_Surface	*arena;
   SDL_Rect	a;
   SDL_Rect	b;
+  SDL_Rect	position;
 
   if (SDL_Init(SDL_INIT_VIDEO) == -1)
     return (1);
-  //  if (TTF_Init() == -1)
-  //return (1);
   if ((screen = SDL_SetVideoMode(1000, 1000, 32, SDL_SWSURFACE)) == NULL)
     return (1);
   SDL_WM_SetCaption("Lem-In", NULL);
-  SDL_FillRect(screen, NULL,
-	       SDL_MapRGB(screen->format, 150, 150, 150));
-  a.x = 0;
-  a.y = 0;
+  arena = SDL_CreateRGBSurface(SDL_HWSURFACE, /*1000*/1500, 1000, 32, 0, 0, 0, 0);
+  fill_arena(arena, /*1000*/1500, 1000);
+  a.x = 1450;
+  //a.x = 950;
+  a.y = 150;
+  b.x = 800;
+  b.y = 150;
+  draw_tubes(&a, &b, arena);
+  b.x = 800;
+  b.y = 350;
+  draw_tubes(&a, &b, arena);
+  a.x = 800;
+  a.y = 350;
+  b.x = 800;
+  b.y = 250;
+  draw_tubes(&a, &b, arena);
+  b.x = 450;
+  b.y = 150;
+  draw_tubes(&a, &b, arena);
+  b.x = 200;
+  b.y = 400;
+  draw_tubes(&a, &b, arena);
+  a.x = 800;
+  a.y = 150;
+  b.x = 800;
+  b.y = 250;
+  draw_tubes(&a, &b, arena);
+  b.x = 450;
+  b.y = 150;
+  draw_tubes(&a, &b, arena);
+  a.x = 800;
+  a.y = 250;
+  b.x = 450;
+  b.y = 250;
+  draw_tubes(&a, &b, arena);
+  a.x = 450;
+  a.y = 150;
   b.x = 50;
-  b.y = 50;
-  draw_tubes(&a, &b, screen);
-  draw_circle(0xFF00FF, screen, 0, 0);
-  draw_circle(0xFF0000, screen, 50, 50);
+  b.y = 250;
+  draw_tubes(&a, &b, arena);
+  a.x = 50;
+  a.y = 250;
+  b.x = 200;
+  b.y = 400;
+  draw_tubes(&a, &b, arena);
+  b.x = 450;
+  b.y = 250;
+  draw_tubes(&a, &b, arena);
+  //draw_circle(0xFF0000, arena, 950, 150);
+  draw_circle(0xFF0000, arena, 1450, 150);
+  draw_circle(0xFFFFFF, arena, 800, 350);
+  draw_circle(0xFFFFFF, arena, 800, 150);
+  draw_circle(0xFFFFFF, arena, 800, 250);
+  draw_circle(0xFFFFFF, arena, 450, 150);
+  draw_circle(0xFFFFFF, arena, 50, 250);
+  draw_circle(0xFFFFFF, arena, 200, 400);
+  draw_circle(0x00FF00, arena, 450, 250);
+  position.x = 0;
+  position.y = 0;
+  SDL_BlitSurface(arena, NULL, screen, &position);
   SDL_Flip(screen);
-  my_pause(screen);
+  my_pause(screen, arena, 0, 0, /*1000*/1500, 1000);
   return (0);
 }
 
 int	main()
 {
   init_graphic();
-  //  pause();
   return (0);
 }
