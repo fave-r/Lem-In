032788fcc86f5cf9d0967a5c7fb90c4dd61e6299
odet_a <odet_a@epitech.eu>
2014-04-30 17:46:25 +0200

    Normalement ça va marcher

--- graphic/parsing/parse.c
+++ graphic/parsing/parse.c
@@ -5,21 +5,25 @@
 ** Login   <Alex@epitech.net>
 ** 
 ** Started on  Wed Apr 30 16:09:14 2014 Alex
-** Last update Wed Apr 30 16:50:50 2014 Thibaut Lopez
+** Last update Wed Apr 30 16:37:33 2014 Alex
 */
 
-#include "graphic.h"
+#include "lem_in.h"
 
-t_all	*parse(char **map)
+t_all	*parse()
 {
   int	i;
   t_all	*list;
 
   i = 1;
   list = xmalloc(sizeof(t_all));
-  list->room = parse_room(map, &i);
+  list->map = init_parse();
+  if (list->map == NULL)
+    no_map();
+  list->ants = parse_ants(list->map[0]);
+  list->room = parse_room(list->map, &i);
   list->room = fill_list_num(list->room);
-  list->arc = parse_arc(list->room, &i, map);
+  list->arc = parse_arc(list->room, &i, list->map);
   if (list->arc == NULL)
     no_arc();
   list->arc = arc_num(list->arc, list->room);
